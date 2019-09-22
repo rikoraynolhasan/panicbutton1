cff1859d7b247c805c5e85624b27b75d8502ef77
Desnita Aspriyani <desnitaaspriyani98@gmail.com>
2019-09-22 13:50:14 +0800

    desnita 2

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,13 +1,17 @@
 <li class="{{ Request::is('incidents*') ? 'active' : '' }}">
-    <a href="{!! route('incidents.index') !!}"><i class="fa fa-edit"></i><span>Incidents</span></a>
+    <a href="{!! route('incidents.index') !!}"><i class="fa fa-edit"></i><span>Incident</span></a>
 </li>
 
 <li class="{{ Request::is('dataUsers*') ? 'active' : '' }}">
-    <a href="{!! route('dataUsers.index') !!}"><i class="fa fa-edit"></i><span>Data Users</span></a>
+    <a href="{!! route('dataUsers.index') !!}"><i class="fa fa-edit"></i><span>Data User</span></a>
 </li>
 
 <li class="{{ Request::is('dataUsers*') ? 'active' : '' }}">
     <a href="/maps"><i class="fa fa-edit"></i><span>Maps</span></a>
+</li>
+
+<li class="{{ Request::is('dataUsers*') ? 'active' : '' }}">
+    <a href="/maps"><i class="fa fa-edit"></i><span>Bantuan</span></a>
 </li>
 
 
